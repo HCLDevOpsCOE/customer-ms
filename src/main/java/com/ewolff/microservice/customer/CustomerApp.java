546e0a694e5eb3d944672aef322172736f9963fb
hrishikesh.deodhar <hrishikesh.deodhar@hcl.com>
2017-03-03 14:43:53 +0530

    Configure Public DNS on AWS for the Client

--- src/main/java/com/ewolff/microservice/customer/CustomerApp.java
+++ src/main/java/com/ewolff/microservice/customer/CustomerApp.java
@@ -3,11 +3,18 @@ package com.ewolff.microservice.customer;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -17,25 +24,33 @@ public class CustomerApp {
 
 	private final CustomerRepository customerRepository;
 
+	@Value("${server.port}") 
+	private int port;
+	
+	@Value("${spring.application.name}")
+	private String name;
+	
 	@Autowired
 	public CustomerApp(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
 
-//	@PostConstruct
-//	public void generateTestData() {
-//		customerRepository.save(new Customer("Lionel", "Messi",
-//				"lionel.messi@gmail.com", "Barcelona Ave", "Argentina"));
-//		customerRepository.save(new Customer("Phunsuk", "Wangdu",
-//				"phunsuk.wangdu@3idiots.com", "Leh Ave", "Leh"));
-//		customerRepository.save(new Customer("Hrishikesh", "Deodhar",
-//				"hr1sh1kesh.deodhar@devOpsEngineers.com", "Electronics City", "Bangalore"));
-//		customerRepository.save(new Customer("Jessie", "James",
-//				"Jessie.James@gmail.com", "Electronics City", "Bangalore"));
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApp.class, args);
 	}
 
+	@Bean
+	@Profile(value="aws")
+    public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+	    EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+	    AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild(name);
+	    config.setDataCenterInfo(info);
+	    info.getMetadata()
+	            .put(AmazonInfo.MetaDataKey.publicHostname.getName(), info.get(AmazonInfo.MetaDataKey.publicHostname));
+	    config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+	    config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	    config.setNonSecurePort(port);
+	    return config;
+	}
+	
 }
